edf269da53608e0ff8a01ddef29930bfe30764f0
wendem03 <nikolay.olegovich03@gmail.com>
2021-06-21 06:02:42 +0300

    dsadsA

--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,7 @@ let path = {
     },
     watch: {
         html: source_folder + "/**/*.html",
-        css: source_folder + "/scss/*.scss",
+        css: source_folder + "/scss/**/*.scss",
         js: source_folder + "/js/*.js",
         img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}"
     },
